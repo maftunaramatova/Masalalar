88ad46b17f759b630c07b05f4ee04e9edf315955
senyoramoon010 <djumaniyazovumarjon@gmail.com>
2025-10-03 18:04:33 +0500

    Add files via upload

---
+++ masala2.py
@@ -0,0 +1,147 @@
+## I. Oddiy matematik masalalar
+# # 1-masala:
+# a = int(input("1-butun sonni kiriting:"))
+# b = int(input("2-butun sonni kiriting:"))
+# suma = a + b
+# print(f"Natija: {suma}")
+
+##2-Masala:
+# I = int(input("To'rtburchak uzunligini kiriting:"))
+# W = int(input("To'rtburchak Enini kiritining"))
+# area = I * W
+# print(f"Natija: {area}")
+
+##3-Masala:
+# x = int(input("1-sonni kiriting:: "))
+# y = int(input("2-sonni kiriting:: "))
+# z = int(input("3-sonni kiriting:: "))
+# avg = (x+y+z) / 3
+# print(f"Natija: {avg}")
+
+##4-Masala:
+# a = int(input("1-uzunligni kiriting: "))
+# b = int(input("2-uzunligni kiriting: "))
+# p = 2 * (a + b)
+# print(f"Natija: {p}")
+
+##5-Masala:
+# h = int(input("Soat kiriting:"))
+# m = int(input("Minut kiriting:"))
+# total_min=h * 60 + m
+# print(f"Natija: {total_min}")
+
+##6-Masala:
+# usd = int(input("Dollar miqdori : "))
+# sum = usd * 12000
+# print(f"Natija: {sum}")
+
+##7-Masala:
+# a = int(input("1-Sonni kiriting:"))
+# b = int(input("2-Sonni kiriting:"))
+# diff = abs(a - b)
+# print(f"Natija: {diff}")
+
+##8-Masala:
+# x = int(input("Asos kiriting(son):"))
+# n = int(input("Daraja(son):"))
+# power = x ** n
+# print(f"Natija: {power}")
+
+##9-Masala:
+# a = int(input("1-Sonni kiriting: "))
+# b = int(input("2-Sonni kiriting: "))
+# quotient = a // b
+# print(f"Natija: {quotient}")
+
+##10-Masala:
+# a = int(input("1-Sonni kiriting:"))
+# b = int(input("2-Sonni kiriting:"))
+# almashtirish = a, b = b, a
+# print("Natija: {almashtirish}")
+
+
+## II. Hayotiy muammolar
+
+##1-Masala:
+# total_rent = int(input("Uy ijarasining to'lovi so'mda : "))
+# n = int(input("Uy ijarasining oyini kiriting : "))
+# per_month = total_rent / n
+# print(f"Natija: {per_month}")
+
+##2-Masala:
+# d = int(input("Masofa kiriting: "))
+# v = int(input("O'rtacha tezlik kiriting: "))
+# time = d / v
+# print(f"Natija : {time}")
+
+##3-Masala:
+# seats = int(input("O'rindiqlar soni :"))
+# passengers  = int(input("Yo'lovchilar kelgan soni :"))
+# per_person = passengers // seats
+# left = passengers % seats
+# print(f"Natija: {per_person}")
+
+##4-Masala;
+# consumption = int(input("Yoqilg'i:"))
+# d = int(input("Masofa:"))
+# fuel = consumption * d / 100
+# print(f"Natija: {fuel}")
+
+##5-Masala:
+# salary = float(input("Brutto maoshi: "))
+# net = salary * (1 - 0.12)
+# print(f"Natija: {net}")
+
+##6-Masala:
+# amount = int(input("Miqdor summa: "))
+# days = int(input(" Har kechikkan kun uchun 0.1% jarima: "))
+# penalty = amount * 0.001 * days
+# total = amount + penalty
+# print(f"Jami to'lov: {total}")
+
+##7-Masala:
+# price = int(input("Mahsulot narxi: "))
+# discount = int(input("Chegirma(%) kiriting:"))
+# paid = price * (1 - discount / 100)
+# print(f"To'lanadigan summa:{paid}")
+
+##8-Masala:
+# P = int(input("Asos kiriting:"))
+# r = int(input("Foiz stavkasi: "))
+# t = int(input("Yillar: "))
+# tinterest = P * r / 100 * t
+# print(f"Jami summa : {tinterest}")
+
+##9-Masala:
+# base_qty = int(input("Asosiy miqdor grammda: "))
+# people = int(input("Kishilar soni: "))
+# per_person = base_qty / 4
+# needed = per_person * people
+# print(f"Asosiy miqdor grammda: {base_qty} ga teng ,Kishilar soni: {needed} ga teng")
+
+##10-Masala:
+# bill = int(input("Hisob kitob summasi :"))
+# tip = bill * 0.10
+# total = bill + tip
+# print(f"Tip : {tip}, Jami to'lov: {total}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
